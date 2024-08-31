83cf86b74e827c89b19f29dd0f583aa4701759bd
Oluwapelumi Israel <oluwapelumisotoyinbo@gmail.com>
2024-08-31 13:25:32 +0100

    aws for media

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,14 @@
-module.exports = () => ({});
+// module.exports = () => ({});
+module.exports = ({ env }) => ({
+  upload: {
+    provider: 'aws-s3', // or 'cloudinary', etc.
+    providerOptions: {
+      accessKeyId: env('AWS_ACCESS_KEY_ID'),
+      secretAccessKey: env('AWS_ACCESS_SECRET'),
+      region: env('AWS_REGION'),
+      params: {
+        Bucket: env('AWS_BUCKET'),
+      },
+    },
+  },
+});
