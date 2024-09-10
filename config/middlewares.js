9aeadc3b61f8c67a3b3e8e2f84707b816e8755ba
Oluwapelumi Israel <oluwapelumisotoyinbo@gmail.com>
2024-09-10 08:57:21 +0100

    middleware

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,25 +1,12 @@
-// module.exports = [
-//   'strapi::logger',
-//   'strapi::errors',
-//   'strapi::security',
-//   'strapi::cors',
-//   'strapi::poweredBy',
-//   'strapi::query',
-//   'strapi::body',
-//   'strapi::session',
-//   'strapi::favicon',
-//   'strapi::public',
-// ];
-const cache = require('koa-cache');
-
-
-module.exports = ({ env }) => ({
-  settings: {
-    cache: {
-      enabled: true,
-      models: ['article', 'user'], // Specify the models you want to cache
-      type: 'memory',
-      maxAge: 60000,
-    },
-  },
-});
+module.exports = [
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
