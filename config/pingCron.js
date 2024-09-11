5876fcca2920894b62be473d9b9b9a39a08684dc
Oluwapelumi Israel <oluwapelumisotoyinbo@gmail.com>
2024-09-11 14:06:20 +0100

    added node-cron

---
+++ config/pingCron.js
@@ -0,0 +1,17 @@
+const cron = require('node-cron');
+const axios = require('axios');
+
+const https = require('https');
+const agent = new https.Agent({
+  secureProtocol: 'TLSv1_2_method',
+});
+
+// Schedule the job to run every 5 minutes
+cron.schedule('*/5 * * * *', async () => {
+  try {
+    await axios.get('https://gregory-medical-journal-backend.onrender.com/api/articles/');
+    console.log('Ping successful!');
+  } catch (error) {
+    console.error('Ping failed:', error.message);
+  }
+});
